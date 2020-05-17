2feea54b2ed54a805c4826ab1c5d38454b4fe5ae
apoyezzhayev <arseniy.poyezzhayev@gmail.com>
2020-05-17 23:42:40 +0300

    Add logging message to test.py

--- test.py
+++ test.py
@@ -3,6 +3,7 @@ from utils.lane import LaneSimulation
 
 import traci.constants as tc
 import traci
+from tqdm import tqdm
 
 # constants are here: https://github.com/eclipse/sumo/blob/master/tools/traci/constants.py
 
@@ -14,13 +15,14 @@ import traci
 from utils.vehicles import VehiclesSimulation
 
 
-def run(run_time):
-    with Simulation(options, run_time=run_time, no_gui=False, dt=0.1) as sim:
+def run(run_time, opts):
+    with Simulation(opts, run_time=run_time, no_gui=opts.nogui, dt=0.1) as sim:
         lanes_sim = LaneSimulation(dv_dr='dvdr_gm.pickle')
-        vehicles_sim = VehiclesSimulation(lanes_sim.lanes, model='platoon')
+        vehicles_sim = VehiclesSimulation(lanes_sim.lanes, model=opts.m)
         # Get lanes
         for step, t in sim:
-            # print('Step %d' % step)
+            if (step % 100) == 0:
+                tqdm.write('Step %d: number of vehicles = %d' % (step, traci.simulation.getMinExpectedNumber()))
             lanes_sim.step()
             vehicles_sim.step()
             # Add new vehciles to subscription
@@ -36,7 +38,8 @@ def run(run_time):
 
 if __name__ == "__main__":
     parser = sumo_basic_parser()
-    options = parser.parse_args('-c maryland/testmap.sumocfg'.split(' '))
+    options = parser.parse_args()
+    # options = parser.parse_args('-c maryland/testmap.sumocfg -m platoon --nogui'.split(' '))
     # options = parser.parse_args('-c ../lab_1/inno.sumocfg'.split(' '))
     # options = parser.parse_args('-c single_lane/hello.sumocfg'.split(' '))
-    run(run_time=1000)
+    run(run_time=options.s, opts=options)
